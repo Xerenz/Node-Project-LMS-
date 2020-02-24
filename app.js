ea3a0bfe443e125b44aebe1b7445a4aceb4444d8
Ajal333 <ajapoos1999@gmail.com>
2020-02-24 14:46:09 +0530

    Update

--- app.js
+++ app.js
@@ -6,12 +6,13 @@ const server = express();
 
 mongoose.connect("mongodb://localhost/library");
 
-server.set('view enginer','ejs');
+server.set("view engine","ejs");
 
 server.use("/assets/css", express.static(__dirname + "/assets/css"));
 
 server.get('/',(req,res) => {
     res.render('home');
+    console.log("Yeah!!");
 });
 
 server.listen(8000);
